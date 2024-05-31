80116b06bdb53cc6f2273167a37e03922f6893c3
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-05-31 13:45:50 +0300

    fix(dasboard order) incorrect page title and search placeholder

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -5,15 +5,15 @@ import OrderTable from '../tables/OrderTable';
 const Page = () => {
   return (
     <div className="w-4/5">
-      <h1 className="mb-10 text-4xl">Категорії</h1>
+      <h1 className="mb-10 text-4xl">Замовлення</h1>
       <div className="mb-10 flex justify-between">
-        <Search placeholder="Назва категорії" />
-        <Link
+        <Search placeholder="Логін або номер замовлення" />
+        {/* <Link
           href={'/dashboard/categories/create'}
           className="rounded-xl bg-blue-400 p-4 text-lg text-white transition-colors hover:bg-blue-700"
         >
           Створити нову категорію
-        </Link>
+        </Link> */}
       </div>
       <OrderTable />
     </div>
