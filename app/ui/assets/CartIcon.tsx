34e1e3122abd82fa25486dcf68c303dbcf047844
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-07-04 21:51:57 +0300

    Create CartIcon.tsx

---
+++ app/ui/assets/CartIcon.tsx
@@ -0,0 +1,11 @@
+import cartSvg from '../../assets/cart.svg';
+
+const CartIcon = () => {
+  return (
+    <svg className="" width="" height="">
+      <use href={cartSvg}></use>
+    </svg>
+  );
+};
+
+export default CartIcon;
