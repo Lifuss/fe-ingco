c824079789b4014cf0a4a229e65231cdc66cf01e
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-07-13 23:32:02 +0300

    feature(error) create error.tsx for shop page

--- app/shop/error.tsx
+++ app/shop/error.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
 
 export const error = () => {
-  return <div>В</div>;
+  return (
+    <div className="mx-auto flex flex-col items-center gap-4">
+      <h2 className="text-2xl">Щось пішло не так</h2>
+      <Link
+        className="w-fit rounded-md bg-orangeLight p-2 text-lg transition-colors hover:bg-orange-400"
+        href="/"
+      >
+        На головну
+      </Link>
+    </div>
+  );
 };
+
+export default error;
