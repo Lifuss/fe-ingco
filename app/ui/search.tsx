b3df3e9567e163943104a56c5709761791fc86d2
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-09-27 13:25:13 +0300

    fix(search) able to search from product page & some upgrades

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,24 +1,29 @@
 'use client';
-
 import { Search } from 'lucide-react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useRef } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function SearchFoo({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const searchQuery = searchParams?.get('query')?.toString() || '';
+
+  // "66" is unique first part of id for products, and we need go out from product page
+  const validPathname = pathname.includes('66')
+    ? pathname.slice(0, pathname.indexOf('66') - 1)
+    : pathname;
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams.toString());
+
     params.set('page', '1');
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }, 500);
+    term ? params.set('query', term) : params.delete('query');
+
+    replace(`${validPathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex w-fit flex-shrink-0 xl:w-2/5">
@@ -26,14 +31,17 @@ export default function SearchFoo({ placeholder }: { placeholder: string }) {
         Пошук
       </label>
       <input
-        className="peer block w-full rounded-2xl border border-black bg-transparent py-[4px] pl-10 text-base outline-2 transition-colors placeholder:text-[#717171] focus:border-gray-300 focus:bg-gray-200 focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 lg:py-[13px]"
+        className="block w-full rounded-2xl border border-black bg-transparent py-[4px] pl-10 text-base outline-2 transition-colors placeholder:text-[#717171] focus:border-gray-300 focus:bg-gray-200 focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 lg:py-[13px]"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchQuery}
+        aria-label="Search"
+        ref={inputRef}
       />
       <Search
         size={20}
         className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+        onClick={() => inputRef.current?.focus()}
       />
     </div>
   );
