936300a6ff34fc67b2111fbd4dcda3460bfdc0b4
Arsen Hryhoriak <arsen.hryhoriak@develops.today>
2025-05-29 17:57:11 +0300

    migrate to new api

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['localhost', 'be-ingco.store'],
+    domains: ['localhost', 'be-ingco.store', 'api-ingco-service.win'],
   },
 };
 
