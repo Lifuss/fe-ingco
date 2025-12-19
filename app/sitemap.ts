ac331f85c6affa76cd91546a419f3d88303d5122
Arsen Hryhoriak <arsen.hryhoriak@develops.today>
2025-12-19 12:45:53 +0200

    refactor: update changeFrequency type in sitemap function and remove deprecated API route for sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -82,7 +82,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       return {
         url: `${DOMAIN}/${productSlug}`,
         lastModified: currentDate,
-        changeFrequency: 'weekly',
+        changeFrequency: 'weekly' as const,
         priority: 0.8,
       };
     },
